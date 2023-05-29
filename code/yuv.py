22ba2f658bb4cc86a650e24e45bb6e1b6bf53b36
SofianeM <sofiane.mami@laposte.net>
2023-05-29 19:17:16 +0200

    modif using np array

--- code/yuv.py
+++ code/yuv.py
@@ -1,15 +1,13 @@
 #Module transforming RGB images into YUV
+import numpy as np
 
-yuv = [[0.299,0.587,0.114],
-       [-0.147,-0.289,0.436],
-       [0.615,-0.407,-0.079]]
+
+yuv = np.array([[0.299,0.587,0.114],
+                [-0.147,-0.289,0.436],
+                [0.615,-0.407,-0.079]])
 
 
 def rgb_to_yuv(rgb:tuple) -> tuple:
-    r = rgb[0]
-    g = rgb[1]
-    b = rgb[2]
-    y = yuv[0][0] * r + yuv[0][1] * g + yuv[0][2] * b
-    u = yuv[1][0] * r + yuv[1][1] * g + yuv[1][2] * b
-    v = yuv[2][0] * r + yuv[2][1] * g + yuv[2][2] * b
-    return (y,u,v)
+       a = np.asarray(rgb)
+       b = yuv.dot(a)
+       return tuple(b)
